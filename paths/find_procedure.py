ecfb0bb14d040e9f9df11bcd464096cfd23d8245
Feelsever <philipp.bhandari@gmail.com>
2018-11-24 00:02:48 +0300

    first done

--- paths/find_procedure.py
+++ paths/find_procedure.py
@@ -1,45 +1,37 @@
-# Задание
-# мне нужно отыскать файл среди десятков других
-# я знаю некоторые части этого файла (на память или из другого источника)
-# я ищу только среди .sql файлов
-# 1. программа ожидает строку, которую будет искать (input())
-# после того, как строка введена, программа ищет её во всех файлах
-# выводит список найденных файлов построчно
-# выводит количество найденных файлов
-# 2. снова ожидает ввод
-# поиск происходит только среди найденных на этапе 1
-# 3. снова ожидает ввод
-# ...
-# Выход из программы программировать не нужно.
-# Достаточно принудительно остановить, для этого можете нажать Ctrl + C
-
-# Пример на настоящих данных
-
-# python3 find_procedure.py
-# Введите строку: INSERT
-# ... большой список файлов ...
-# Всего: 301
-# Введите строку: APPLICATION_SETUP
-# ... большой список файлов ...
-# Всего: 26
-# Введите строку: A400M
-# ... большой список файлов ...
-# Всего: 17
-# Введите строку: 0.0
-# Migrations/000_PSE_Application_setup.sql
-# Migrations/100_1-32_PSE_Application_setup.sql
-# Всего: 2
-# Введите строку: 2.0
-# Migrations/000_PSE_Application_setup.sql
-# Всего: 1
-
-# не забываем организовывать собственный код в функции
-
 import os
+import glob
+import subprocess
 
-migrations = 'Migrations'
 current_dir = os.path.dirname(os.path.abspath(__file__))
+migrations = current_dir + '/Migrations'
+
+
+def check_file(file, query):
+    process = subprocess.Popen('cat "{}" | grep "{}"'.format(file, query), shell=True, stdout=subprocess.PIPE)
+    stdout_list = process.communicate()
+    return bool(stdout_list[0])
+
+
+def use_search(files):
+    print('Введите строку:')
+    query = input()
+    files_list = []
+    for file in files:
+        filename = os.path.join(migrations, file)
+        result = check_file(filename, query)
+        if result == 1:
+            files_list.append(file)
+            print(file)
+    print('Количество подходящих файлов: {}'.format(len(files_list)))
+    return files_list
+
 
 if __name__ == '__main__':
-    # ваша логика
-    pass
+
+    os.chdir(migrations)
+    all_files = glob.glob('*.sql')
+
+    temp_files = use_search(all_files)
+
+    while len(temp_files) > 1:
+        temp_files = use_search(temp_files)
