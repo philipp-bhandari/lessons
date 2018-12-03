7c63fc76c7f445dc8d65c3ccee25cb91468af358
feelsever <philipp.bhandari@gmail.com>
2018-12-03 17:09:32 +0300

    Доработки
    Signed-off-by: feelsever <philipp.bhandari@gmail.com>

--- files/files.py
+++ files/files.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+
+
 def split_line(line):
     line_arr = line.split('|')
     line_arr = [i.strip() for i in line_arr]
@@ -10,22 +13,20 @@ def get_string(file):
 
 def is_ingredient(line, index):
     try:
-        if line[index] == 1:  # Не придумал другого способа проверить существует ли элемент списка.
+        if line[index]:  # Не придумал другого способа проверить существует ли элемент списка.
             pass
         return True
     except IndexError:
         return False
 
 
-def exercise_1():
+def read_cookbook_file():
     cook_book = {}
-    dish_persons_count = {}
     with open('cookbook') as file:
-
         for line in file:
             cook_name = line.strip()
 
-            dish_persons_count[cook_name] = get_string(file)  # Для второго задания
+            get_string(file)  # Пропуск строки с количеством ингредиентов
 
             ingredient_line = split_line(get_string(file))
 
@@ -37,31 +38,16 @@ def exercise_1():
                 cook_book[cook_name].append(ingredient)
                 ingredient_line = split_line(get_string(file))
             continue
-    return cook_book, dish_persons_count
+    return cook_book
 
 
 def get_shop_list_by_dishes(dishes, person_count):
-    result = exercise_1()
-    cook_book = result[0]
-    dish_persons_count = result[1]
-
-    new_cook_book = {}  # Кукбук с количеством ингредиентов для одного человека
-
-    for cook, ingredients in cook_book.items():
-        for dish, cook_book_person_count in dish_persons_count.items():
-            if cook == dish:
-                new_cook_book[dish] = []
-                for ingredient in ingredients:
-                    new_ingredient = {'ingredient_name': ingredient['ingredient_name'],
-                                      'quantity': float(ingredient['quantity']) / int(cook_book_person_count),
-                                      'measure': ingredient['measure']}
-                    new_cook_book[dish].append(new_ingredient)
-
+    cook_book = read_cookbook_file()
     out_dict = {}
 
     for dish in dishes:
         try:
-            for ingredient in new_cook_book[dish]:
+            for ingredient in cook_book[dish]:
                 name = ingredient['ingredient_name']
 
                 if name not in out_dict:
@@ -69,7 +55,7 @@ def get_shop_list_by_dishes(dishes, person_count):
                     out_dict[name] = {'measure': ingredient['measure'],
                                       'quantity': quantity}
                 else:
-                    quantity = out_dict[name]['quantity'] + (ingredient['quantity'] * int(person_count))
+                    quantity = out_dict[name]['quantity'] + (float(ingredient['quantity']) * int(person_count))
                     out_dict[name] = {'measure': ingredient['measure'],
                                       'quantity': quantity}
 
@@ -78,9 +64,8 @@ def get_shop_list_by_dishes(dishes, person_count):
     return out_dict
 
 
-def show_result_1():
-    result = exercise_1()
-    cook_book = result[0]
+def print_cook_book():
+    cook_book = read_cookbook_file()
 
     for cook, ingredients in cook_book.items():
         print(cook)
@@ -88,14 +73,15 @@ def show_result_1():
             print(ingredient)
         print('\n' + '*' * 3 + '\n')
 
-def show_result_2():
-    result = get_shop_list_by_dishes(['Фахитос', 'Омлет', 'Утка по-пекински', 'Запеченный картофель', 'Запеченный картофель'], 1)
+
+def print_quantity(dishes, count):
+    result = get_shop_list_by_dishes(dishes, count)
     for item, props in result.items():
-        print(item)
-        print(props)
+        pprint(item)
+        pprint(props)
 
 
+print_cook_book()  # Задание 1
 
-#show_result_1()  # Раскомментируйте, чтобы вывести результат
 
-show_result_2()
+print_quantity(['Фахитос', 'Омлет', 'Утка по-пекински'], 4)  # Задание 2
